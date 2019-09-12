e93da3643a4cdf0725e868eed4b03d344470e418
DanielClough <danielclough@protonmail.com>
2019-09-12 01:48:38 -0700

    update

---
+++ assets/js/yukino.js
@@ -0,0 +1,24 @@
+eqfeed_callback({"type":"FeatureCollection","features":[{"type":"Venue",
+	"properties":{
+		"hot":true,
+		"iced":null,
+		"nitro":null,
+		"volume-of-tea": 1,
+		"profit-margin": "high/med/low",
+		"busy-time":"T18:25",
+		"slow-time":"T18:25"
+		"url":"https://venue.com",
+		"last-visit":"2019-08-23T18:25:43.511Z", /*date - last 5 for feel-plan:brand-service-quality*/
+		"next-visit":"2019-08-23T18:25:43.511Z", /*date - last 5 for expectation-plan:importance-profit-pressure*/
+		"notes":"",
+		"current-teas":["HCS", "DPJ", "Paris"],
+		"other-companies":"Art of Tea, Song Tea",
+		"coffee-deal":true
+	},
+
+	"geometry":{
+		"address":"",
+		"type":"Point",
+		"coordinates":[126.3832,5.9775,111.16]},
+		"id":"usc000csx3"}]}	)
+
